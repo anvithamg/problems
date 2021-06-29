cfb72e5729b9f7175389950ad7690c85a49709be
anvithamg <anvithamg2001@gmail.com>
2021-06-29 20:06:42 +0530

    Solved leetcode problem 94

---
+++ leetcode/94.java
@@ -0,0 +1,61 @@
+/*94. Binary Tree Inorder Traversal
+Easy
+
+Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+ 
+
+Example 1:
+
+Input: root = [1,null,2,3]
+Output: [1,3,2]
+
+Example 2:
+
+Input: root = []
+Output: []
+
+Example 3:
+
+Input: root = [1]
+Output: [1]
+
+Example 4:
+
+Input: root = [1,2]
+Output: [2,1]
+
+Example 5:
+
+Input: root = [1,null,2]
+Output: [1,2]*/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+        ArrayList<Integer> list=new ArrayList<Integer>();
+    public List<Integer> inorderTraversal(TreeNode root) {
+        inorder(root);
+        return list;
+        
+    }
+    public void inorder(TreeNode root) {
+        if(root==null)
+            return;
+        inorder(root.left);
+        list.add(root.val);
+        inorder(root.right);
+    }
+}
