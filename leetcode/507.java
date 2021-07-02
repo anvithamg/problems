3d706bf859d73545cfa3fa2b9425a20e8839bf37
anvithamg <anvithamg2001@gmail.com>
2021-07-02 09:27:28 +0530

    Solved leetcode problem 367

--- leetcode/507.java
+++ leetcode/507.java
@@ -36,11 +36,11 @@ Output: false*/
 class Solution {
     public boolean checkPerfectNumber(int num) {
         int count = 0;
-        for(int i=1;i<=num/2;i++) {
-            if(num % i==0) 
+        for (int i = 1; i <= num / 2; i++) {
+            if (num % i == 0)
                 count = count + i;
         }
-        if(count == num)
+        if (count == num)
             return true;
         return false;
     }
