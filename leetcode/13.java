fd11c22fca69cbd557f45a2de573057bf49c1753
anvithamg <anvithamg2001@gmail.com>
2021-07-11 20:34:16 +0530

    Solved leetcode problem 26

--- leetcode/13.java
+++ leetcode/13.java
@@ -54,37 +54,31 @@ class Solution {
     public int romanToInt(String s) {
         int count = 0;
         int[] nums = new int[s.length()];
-        for (int i = 0; i < s.length(); i++) {
-            switch (s.charAt(i)) {
-                case 'I':
-                    nums[i] = 1;
-                    break;
-                case 'V':
-                    nums[i] = 5;
-                    break;
-                case 'X':
-                    nums[i] = 10;
-                    break;
-                case 'L':
-                    nums[i] = 50;
-                    break;
-                case 'C':
-                    nums[i] = 100;
-                    break;
-                case 'D':
-                    nums[i] = 500;
-                    break;
-                case 'M':
-                    nums[i] = 1000;
-                    break;
+        for(int i=0;i<s.length();i++) {
+            switch(s.charAt(i)) {
+                    case 'I' : nums[i] = 1;
+                         break;
+                    case 'V' : nums[i] = 5;
+                         break;
+                    case 'X' : nums[i] = 10;
+                         break;
+                    case 'L' : nums[i] =  50;
+                         break;
+                    case 'C' : nums[i] = 100;
+                         break;
+                    case 'D' : nums[i] = 500;
+                         break;
+                    case 'M' : nums[i] = 1000;
+                         break;  
             }
         }
-        for (int i = 0; i < nums.length - 1; i++) {
-            if (nums[i] < nums[i + 1])
+        for(int i = 0;i<nums.length-1;i++) {
+            if(nums[i]<nums[i+1]) 
                 count -= nums[i];
             else
-                count += nums[i];
+                count +=nums[i];
         }
-        return count + nums[nums.length - 1];
+        return count+nums[nums.length - 1];
     }
 }
+
