c1a046e0a5de702e11cb65893fe890cd4949ee36
anvithamg <anvithamg2001@gmail.com>
2021-09-06 20:43:50 +0530

    Solved leetcode problem 387

---
+++ leetcode/387.java
@@ -0,0 +1,34 @@
+/*387. First Unique Character in a String
+Easy
+
+Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
+
+ 
+
+Example 1:
+
+Input: s = "leetcode"
+Output: 0
+
+Example 2:
+
+Input: s = "loveleetcode"
+Output: 2
+
+Example 3:
+
+Input: s = "aabb"
+Output: -1*/
+class Solution {
+    public int firstUniqChar(String s) {
+        int[] ch = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            ch[s.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            if (ch[s.charAt(i) - 'a'] == 1)
+                return i;
+        }
+        return -1;
+    }
+}
