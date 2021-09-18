fcd7e78e6caac1fb220a3b3d993c35ab767d15dd
anvithamg <anvithamg2001@gmail.com>
2021-09-18 20:45:23 +0530

    Solved leetcode problem 219

---
+++ leetcode/219.java
@@ -0,0 +1,35 @@
+/*219. Contains Duplicate II
+Easy
+
+Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,1], k = 3
+Output: true
+
+Example 2:
+
+Input: nums = [1,0,1,1], k = 1
+Output: true
+
+Example 3:
+
+Input: nums = [1,2,3,1,2,3], k = 2
+Output: false*/
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (hashMap.containsKey(nums[i])) {
+                if (i - hashMap.get(nums[i]) <= k) {
+                    return true;
+                }
+            }
+            hashMap.put(nums[i], i);
+        }
+        return false;
+    }
+}
